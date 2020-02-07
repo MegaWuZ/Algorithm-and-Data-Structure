65bb72c4fdd8657f6c15c0ce7980b72df914d8ee
KrisWuuu <MegaWuL@163.com>
2020-02-07 09:55:17 +0800

    Rename InsertionSort.h to Basic-Sorting/InsertionSort.h

--- Basic-Sorting/InsertionSort.h
+++ Basic-Sorting/InsertionSort.h
@@ -8,7 +8,7 @@ using namespace std;
 template<typename T>
 void insertionSort(T arr[], int n) {
 	for (int i = 1; i < n; i++) {
-		//Ñ°ÕÒÔªËØarr[i]ºÏÊÊµÄ²åÈëÎ»ÖÃ//Ñ°ÕÒÔªËØarr[i]ºÏÊÊµÄ²åÈëÎ»ÖÃ
+		//å¯»æ‰¾å…ƒç´ arr[i]åˆé€‚çš„æ’å…¥ä½ç½®//å¯»æ‰¾å…ƒç´ arr[i]åˆé€‚çš„æ’å…¥ä½ç½®
 		T e = arr[i];
 		int j;
 		for (j = i; j > 0 && arr[j - 1] > e; j--) {
